569b4eabd97afbbecb8bb251ab7f682541c42761
patrion <patrick.s.davis@gmail.com>
2024-11-17 00:54:11 +0900

    Add multiple routes Fixes #3

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require("express");
 
 const app = express();
 
+const port = 3000;
+
 app.get("/", (request, response) => {
-  response.send("I love Treehouse!");
+  response.send("<h1>I love Treehouse!</h1>");
 });
 
-app.listen(3000);
+app.get("/hello", (request, response) => {
+  response.send("<h1>Hello World!/h1>");
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port:", port);
+});
